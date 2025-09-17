84775ef58c6e89b3c4cde61b4ca9d00ff4adc206
mirajmiah <mirajmiah322173@gmail.com>
2025-09-17 17:16:12 +0530

    gitg

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Config.js
 const CONFIG = {
-    GEMINI_API_KEY: window.location.hostname.includes('localhost') 
-        ? 'AIzaSyAKrUzsPx9eHdz51GlQjGPuOZnPoxh8JXc'  // Local development
-        : 'PLACEHOLDER_FOR_PRODUCTION'  // Will be replaced by backend
+  GEMINI_API_KEY: "AIzaSyAKrUzsPx9eHdz51GlQjGPuOZnPoxh8JXc"
 };
+
+
